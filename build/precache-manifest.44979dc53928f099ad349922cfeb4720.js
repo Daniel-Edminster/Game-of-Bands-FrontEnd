d9c5e2dbe00a38461f8c3da4dfd0a84323959ec3
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-04-24 21:06:48 -0400

    built react app

---
+++ build/precache-manifest.44979dc53928f099ad349922cfeb4720.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798711bb0ac9ca2027dcd5eaed65f9b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38b4a2c0a68eb314a7b",
+    "url": "/static/css/main.b98dab0d.chunk.css"
+  },
+  {
+    "revision": "4cbeec891643552d971e",
+    "url": "/static/js/2.a0479863.chunk.js"
+  },
+  {
+    "revision": "152991639d5e36fa46a6ab37a64657ba",
+    "url": "/static/js/2.a0479863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38b4a2c0a68eb314a7b",
+    "url": "/static/js/main.9aa1d2be.chunk.js"
+  },
+  {
+    "revision": "21e489ef33502d3ae059",
+    "url": "/static/js/runtime-main.eb0afd16.js"
+  },
+  {
+    "revision": "7ef2f3f7f0e1ce10c6f7d169e3dcfb9e",
+    "url": "/static/media/GOBLogo-Clean.7ef2f3f7.png"
+  },
+  {
+    "revision": "832611631ead6604f28376b74eec5499",
+    "url": "/static/media/blurryphones.83261163.jpg"
+  }
+]);
