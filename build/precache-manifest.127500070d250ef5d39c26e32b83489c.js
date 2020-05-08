e816d5cebee515c33035891aaa7b4841a2f8b914
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-05-08 08:34:30 -0400

    fixed link, new build

---
+++ build/precache-manifest.127500070d250ef5d39c26e32b83489c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc09390cb64582a543dfe4e167d56e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aec5820d7b901c7d3f9",
+    "url": "/static/css/main.b98dab0d.chunk.css"
+  },
+  {
+    "revision": "4cbeec891643552d971e",
+    "url": "/static/js/2.a0479863.chunk.js"
+  },
+  {
+    "revision": "152991639d5e36fa46a6ab37a64657ba",
+    "url": "/static/js/2.a0479863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aec5820d7b901c7d3f9",
+    "url": "/static/js/main.81f36106.chunk.js"
+  },
+  {
+    "revision": "21e489ef33502d3ae059",
+    "url": "/static/js/runtime-main.eb0afd16.js"
+  },
+  {
+    "revision": "7ef2f3f7f0e1ce10c6f7d169e3dcfb9e",
+    "url": "/static/media/GOBLogo-Clean.7ef2f3f7.png"
+  },
+  {
+    "revision": "832611631ead6604f28376b74eec5499",
+    "url": "/static/media/blurryphones.83261163.jpg"
+  }
+]);
